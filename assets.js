fb1f478108d1ef48d8d866069f3dc607c931e1e3
Patrick Connolly <patrick.c.connolly@gmail.com>
2021-12-13 18:20:13 -0500

    Migrated jquery and bootstrap to webpack.

--- assets.js
+++ assets.js
@@ -1,7 +1,7 @@
 const CSS = [
   //'font-awesome/css/font-awesome.min.css',
-  //'bootstrap/dist/css/bootstrap.min.css',
-  //'bootstrap/dist/css/bootstrap-theme.min.css',
+  'bootstrap/dist/css/bootstrap.min.css',
+  'bootstrap/dist/css/bootstrap-theme.min.css',
 ];
 
 const JS = [
@@ -9,8 +9,8 @@ const JS = [
   //'fabric.js/dist/fabric.min.js',
   'angular/angular.min.js',
   'angular-route/angular-route.min.js',
-  //'jquery/dist/jquery.min.js',
-  //'bootstrap/dist/js/bootstrap.min.js',
+  'jquery/dist/jquery.min.js',
+  'bootstrap/dist/js/bootstrap.min.js',
 ];
 
 module.exports = [ ...JS, ...CSS ];
