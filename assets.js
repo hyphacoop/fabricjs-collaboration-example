4066dfe78fd16e6e272fc75249713037d808b422
Patrick Connolly <patrick.c.connolly@gmail.com>
2021-12-14 00:30:48 -0500

    Updated bootstrap beyond vuln and added devtool map files.

--- assets.js
+++ assets.js
@@ -1,15 +1,19 @@
 const CSS = [
-  'bootstrap/dist/css/bootstrap.min.css',
   'font-awesome/css/*.min.css',
   'font-awesome/fonts/*',
+  'bootstrap/dist/css/bootstrap.min.css',
+  'bootstrap/dist/css/bootstrap.min.css.map',
   'bootstrap/dist/css/bootstrap-theme.min.css',
+  'bootstrap/dist/css/bootstrap-theme.min.css.map',
 ];
 
 const JS = [
   'socket.io-client/dist/socket.io.min.js',
   'fabric/dist/fabric.min.js',
   'angular/angular.min.js',
+  'angular/angular.min.js.map',
   'angular-route/angular-route.min.js',
+  'angular-route/angular-route.min.js.map',
   'jquery/dist/jquery.min.js',
   'bootstrap/dist/js/bootstrap.min.js',
 ];
